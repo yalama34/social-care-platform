7a63c16ba44a43e5864e8d67938fc1b671101bf6
Эвелина Петрова <evvpetrova@mai.education>
2025-11-06 23:48:35 +0300

    add UserName

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import MainPage from "./components/MainPage"
 import AuthPage from "./components/AuthPage"
 import VerifyCode from "./components/VerifyCode"
+import UserName from "./components/UserName";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App(){
@@ -11,6 +12,7 @@ function App(){
           <Route path="/auth/login" element={<AuthPage />}></Route>
           <Route path="/auth/start-register" element={<AuthPage />}></Route>
           <Route path="/auth/verify-phone" element={<VerifyCode />}></Route>
+          <Route path="/end-registrate" element={<UserName />}></Route>
         </Routes>
     </Router>
   );
